d4f3865afc2fb79331b77c5d6cfba34561bb071c
Edenm <manoreden@gmail.com>
2015-12-12 12:16:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eden/superzol_edenmor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eden.superzol_edenmor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
