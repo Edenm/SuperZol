b31ee2241d7dff347b0a5793ab7c60d13c08da79
Edenm <manoreden@gmail.com>
2015-12-24 19:15:54 +0200

    activity 1

--- app/src/main/java/com/example/eden/superzol_edenmor/SearchActivity.java
+++ app/src/main/java/com/example/eden/superzol_edenmor/SearchActivity.java
@@ -15,14 +15,19 @@ public class SearchActivity extends TabActivity {
 
         mTabHost = getTabHost();
 
-        TabHost.TabSpec ts1 = mTabHost.newTabSpec("page1");
-        ts1.setContent(new Intent(this, Tab1Activity.class));
-        ts1.setIndicator("page1");
-        mTabHost.addTab(ts1);
+        TabHost.TabSpec ts3 = mTabHost.newTabSpec("page1");
+        ts3.setContent(new Intent(this, Tab1Activity.class));
+        ts3.setIndicator("רשימה");
+        mTabHost.addTab(ts3);
 
-        TabHost.TabSpec ts2 = mTabHost.newTabSpec("page2");
-        ts2.setContent(new Intent(this,Tab2Activity.class));
-        ts2.setIndicator("page2");
+        TabHost.TabSpec ts2 = mTabHost.newTabSpec("page1");
+        ts2.setContent(new Intent(this, Tab1Activity.class));
+        ts2.setIndicator("סופר");
         mTabHost.addTab(ts2);
+
+        TabHost.TabSpec ts1 = mTabHost.newTabSpec("page2");
+        ts1.setContent(new Intent(this,Tab1Activity.class));
+        ts1.setIndicator("חיפוש");
+        mTabHost.addTab(ts1);
     }
 }
